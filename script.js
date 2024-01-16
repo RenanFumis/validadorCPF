bce4edf7fafb3893910066d0b85be2cab81f02bf
RenanFumis <renan-oliveira19@hotmail.com>
2024-01-16 19:26:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Arquivo script.js 
+
+import ValidarCpf from './validar-cpf.js';
+
+const cpfElement = document.querySelector('#cpf');
+
+const validarCpf = new ValidarCpf(cpfElement).init();
+
+
---
+++ validar-cpf.js
@@ -0,0 +1,56 @@
+// Arquivo validar-cpf.js 
+
+export default class ValidarCpf{
+
+  constructor(element){
+    this.element = element 
+  }
+  limpar(cpf){
+   return cpf.replace(/\D/g, '')
+  }
+  padronizar(cpf){
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4')
+  }
+  padraoCPFbr(cpf){
+    const cpfLimpo = this.limpar(cpf)
+    return this.padronizar(cpfLimpo)
+  }
+
+  validar(cpf){
+    const matchCPF = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g)
+
+    return (matchCPF && matchCPF[0] === cpf) /*Se matchCPF e matchCPF[0] não forem verdadeiros ele retorna falso*/
+  }
+
+  validaNaMudanca(cpfElement){
+    if(this.validar(cpfElement.value)){
+      cpfElement.value = this.padronizar(cpfElement.value)
+      cpfElement.classList.add('valido')
+      cpfElement.classList.remove('erro')
+      cpfElement.nextElementSibling.classList.remove('ativar')
+    }else{
+      cpfElement.classList.add('erro')
+      cpfElement.classList.remove('valido')
+      cpfElement.nextElementSibling.classList.add('ativar')
+    }
+  }
+
+  adicionarEvento(){
+    this.element.addEventListener('change', () => {
+      this.validaNaMudanca(this.element)
+    })
+  }
+
+  adicionarErroSpan(){
+    const erroElement = document.createElement('span')
+    erroElement.classList.add('erro-text');
+    erroElement.innerText = 'CPF Inválido';
+    this.element.parentElement.insertBefore(erroElement, this.element.nextElementSibling)
+  }
+
+  init(){
+    this.adicionarEvento();
+    this.adicionarErroSpan();
+    return this
+  }
+}
